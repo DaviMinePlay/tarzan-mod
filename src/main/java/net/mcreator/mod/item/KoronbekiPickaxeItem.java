4834e30b9af544dd7f0c4f819200aafebe8243ff
Yuri733 <108159156+Yuri733@users.noreply.github.com>
2023-03-26 13:10:40 -0300

    sexo 3

--- src/main/java/net/mcreator/mod/item/KoronbekiPickaxeItem.java
+++ src/main/java/net/mcreator/mod/item/KoronbekiPickaxeItem.java
@@ -1,28 +1,33 @@
 
 package net.mcreator.mod.item;
 
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.core.BlockPos;
 
+import net.mcreator.mod.procedures.MarretazoioProcedure;
 import net.mcreator.mod.init.ModModItems;
 
 public class KoronbekiPickaxeItem extends PickaxeItem {
 	public KoronbekiPickaxeItem() {
 		super(new Tier() {
 			public int getUses() {
-				return 1164;
+				return 2164;
 			}
 
 			public float getSpeed() {
-				return 12f;
+				return 13f;
 			}
 
 			public float getAttackDamageBonus() {
-				return 4f;
+				return 5.1f;
 			}
 
 			public int getLevel() {
@@ -37,5 +42,12 @@ public class KoronbekiPickaxeItem extends PickaxeItem {
 				return Ingredient.of(new ItemStack(ModModItems.KORONBEKI_INGOT.get()));
 			}
 		}, 1, -3f, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS));
+	}
+
+	@Override
+	public boolean mineBlock(ItemStack itemstack, Level world, BlockState blockstate, BlockPos pos, LivingEntity entity) {
+		boolean retval = super.mineBlock(itemstack, world, blockstate, pos, entity);
+		MarretazoioProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ(), entity, itemstack);
+		return retval;
 	}
 }
